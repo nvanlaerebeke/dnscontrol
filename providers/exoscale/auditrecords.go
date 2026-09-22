8c6196a14ca0f25fb5ad87b0dddca281ab40b946
Tom Limoncelli <tal@whatexit.org>
2026-09-21 22:24:30 -0400

    chore: unify all auditrecords.go files (#4924)

--- providers/exoscale/auditrecords.go
+++ providers/exoscale/auditrecords.go
@@ -10,21 +10,21 @@ import (
 // AuditRecords returns a list of errors corresponding to the records that aren't supported by this provider.
 // If all records are supported, an empty list is returned.
 func AuditRecords(records models.Records) []error {
-	auditor := rejectif.Auditor{}
+	a := rejectif.Auditor{}
 
-	auditor.Add("CAA", rejectif.CaaTargetContainsWhitespace) // Last verified 2022-07-11
+	a.Add("CAA", rejectif.CaaTargetContainsWhitespace) // Last verified 2022-07-11
 
-	auditor.Add("MX", rejectif.MxNull) // Last verified 2022-07-11
+	a.Add("MX", rejectif.MxNull) // Last verified 2022-07-11
 
-	auditor.Add("PTR", func(rc *models.RecordConfig) error {
+	a.Add("PTR", func(rc *models.RecordConfig) error {
 		return errors.New("PTR records are not supported by the Exoscale provider")
 	})
 
-	auditor.Add("SRV", rejectif.SrvHasNullTarget) // Last verified 2020-12-28
+	a.Add("SRV", rejectif.SrvHasNullTarget) // Last verified 2020-12-28
 
-	auditor.Add("TXT", rejectif.TxtHasUnpairedBackslash) // Last verified 2026-05-04
-	auditor.Add("TXT", rejectif.TxtHasDoubleQuotes)      // Last verified 2026-05-04
-	auditor.Add("TXT", rejectif.TxtIsEmpty)              // Last verified 2026-05-04
+	a.Add("TXT", rejectif.TxtHasUnpairedBackslash) // Last verified 2026-05-04
+	a.Add("TXT", rejectif.TxtHasDoubleQuotes)      // Last verified 2026-05-04
+	a.Add("TXT", rejectif.TxtIsEmpty)              // Last verified 2026-05-04
 
-	return auditor.Audit(records)
+	return a.Audit(records)
 }
